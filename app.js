89c7d42b4ec2592753a25fd1abe22f21f96401d1
David Suarez <david.suarez@intelygenz.com>
2015-11-18 11:36:19 +0100

    Request changed to extract dashboard info

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var request = require('request');
 var config = require('config');
+var _ = require('lodash');
 
 server.listen(3000);
 
@@ -19,12 +20,23 @@ io.on('connection', function(socket){
 setInterval(updateData, 500);
 
 function updateData(){
-    request(config.get('nimbus.host') + ":" + config.get('nimbus.port') + '/api/v1/cluster/summary', function(err, result, body) {
+    request(config.get('nimbus.host') + ":" + config.get('nimbus.port') + '/api/v1/topology/summary', function(err, result, body) {
         if (err) return Error(err);
 
-        var nimbusUptime = JSON.parse(body).nimbusUptime;
-        console.log('Result...', nimbusUptime);
-        dataJson = nimbusUptime;
-        io.emit('updateData', nimbusUptime)
+        var topologies = JSON.parse(body).topologies;
+        if(!_.isEmpty(topologies)){
+            topologyId = topologies[0].id;
+
+            request(config.get('nimbus.host') + ":" + config.get('nimbus.port') + '/api/v1/topology/' + topologyId, function(err, result, body) {
+                if (err) return Error(err);
+
+                var bodyParsed = JSON.parse(body).bolts;
+                dataJson = bodyParsed;
+                io.emit('updateData', bodyParsed)
+            });
+
+        } else {
+            io.emit('Error', 'No topology running');
+        }
     });
 }
